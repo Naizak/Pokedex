410ef745da98d8dec2dfd556743f1a2588eba2ab
Naizak <naizakbellemsieh@gmail.com>
2021-11-09 02:19:51 -0500

    Ready for API

---
+++ src/components/Search.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get();
+    };
+
+    search();
+  }, [term]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="form">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
